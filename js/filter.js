1402e5a3c0b6f533c68578d0d5f3246cd0f9731b
Michael Shervashidze <mike.fury315@gmail.com>
2025-05-05 15:33:40 +0700

    Добавил и стилизовал вып список

---
+++ js/filter.js
@@ -0,0 +1,95 @@
+const filterPopup = document.querySelector('.filter-details-popup');
+const filterButtonOpen = document.querySelector('.filter-button-details-open');
+const filterButtonClose = document.querySelector('.filter-button-close');
+
+filterButtonOpen.addEventListener('click', (e) => {
+  filterPopup.style.display = 'block';
+})
+
+filterButtonClose.addEventListener('click', (e) => {
+  filterPopup.style.display = 'none';
+})
+
+function RangeSlider() {
+
+  var parent = document.querySelector("#rangeSlider");
+  if(!parent) return;
+
+  var
+    rangeS = parent.querySelectorAll("input[type=range]"),
+    numberS = parent.querySelectorAll("input[type=number]");
+
+  rangeS.forEach(function(el) {
+    el.oninput = function() {
+      var slide1 = parseFloat(rangeS[0].value),
+        slide2 = parseFloat(rangeS[1].value);
+
+      if (slide1 > slide2) {
+        [slide1, slide2] = [slide2, slide1];
+        // var tmp = slide2;
+        // slide2 = slide1;
+        // slide1 = tmp;
+      }
+
+      numberS[0].value = slide1;
+      numberS[1].value = slide2;
+      console.log(slide1, slide2);
+    }
+  });
+
+  numberS.forEach(function(el) {
+    el.oninput = function() {
+      var number1 = parseFloat(numberS[0].value),
+        number2 = parseFloat(numberS[1].value);
+
+      if (number1 > number2) {
+        var tmp = number1;
+        numberS[0].value = number2;
+        numberS[1].value = tmp;
+      }
+
+      rangeS[0].value = number1;
+      rangeS[1].value = number2;
+
+    }
+  });
+
+}
+
+RangeSlider();
+
+let select = function () {
+  let selectHeader = document.querySelectorAll('.select__header');
+  let selectItem = document.querySelectorAll('.select__item');
+
+  selectHeader.forEach(item => {
+    item.addEventListener('click', selectToggle)
+  });
+
+  selectItem.forEach(item => {
+    item.addEventListener('click', selectChoose)
+  });
+
+  document.addEventListener('click', function(event) {
+    let dropdown = document.querySelector('.select.is-active');
+    if (dropdown && !dropdown.contains(event.target)) {
+      dropdown.classList.remove('is-active');
+    }
+  });
+
+  function selectToggle() {
+    this.parentElement.classList.toggle('is-active');
+  }
+
+  function selectChoose() {
+    let text = this.innerText,
+      select = this.closest('.select'),
+      currentText = select.querySelector('.select__current');
+    currentText.innerText = text;
+    select.classList.remove('is-active');
+    select.classList.add('is-chosen');
+  }
+
+};
+
+select();
