53ed0c3e0bfd90249252bf5e7c5beb7a1b76c864
Michael Shervashidze <mike.fury315@gmail.com>
2025-05-07 20:11:54 +0700

    realise part of ButtonsInput Class 2.0

--- js/filter_class.js
+++ js/filter_class.js
@@ -1,81 +1,90 @@
 // Общий класс для фильтра
 class BaseFilter {
-    sendRequest(url, data) {
-        // return html
-    }
+  sendRequest(url, data) {
+    // return html
+  }
 }
 class BaseInput {
-    constructor(default_value, fclass, fname){
-        this.value = default_value
-        this.fclass = fclass;
-        this.fname = fname;
-    }
-    // get value() {
-    //     return this.value();
-    // }
-    // get request_part() {
+  constructor(default_value, fclass, fname){
+    this.value = default_value
+    this.fclass = fclass;
+    this.fname = fname;
+  }
+  // get value() {
+  //     return this.value();
+  // }
+  // get request_part() {
 
-    // }
+  // }
 }
 
 class ButtonsInput extends BaseInput {
-    constructor(fclass, fname, default_value=null){
-        super(default_value, fclass, fname,);
-        this.fobjects = null;
-        try {
-          if (this.init()){
-            for (let i = 0; i < this.fobjects.length; i++) {
-              this.fobjects[i].addEventListener("click", (e) => {
-                e.target.classList.toggle("filter-button-active");
-                for (let j = 0; j < this.fobjects.length; j++) {
-                  if (i !== j && this.fobjects[j].classList.contains('filter-button-active')){
-                    this.fobjects[j].classList.remove('filter-button-active');
-                  }
-                }
-              })
-            }
-          }
-        } catch (err) {
-          console.log(err);
-        }
+  constructor(fclass, fname, default_value=null){
+    super(default_value, fclass, fname,);
+    this.fobjects = null;
+    if (this.init()){
+      this.initListener();
+    } else {
+      // raise error
     }
-    init(){
-        this.fobjects = document.querySelectorAll(`.${this.fclass}[name="${this.fname}"]`)
-        console.log(this.fobjects)
-        if (this.fobjects.length === 0){
-            return false;
-        }
-        return true;
+
+  }
+  init(){
+    this.fobjects = document.querySelectorAll(`.${this.fclass}[name="${this.fname}"]`)
+    console.log(this.fobjects)
+    if (this.fobjects.length === 0){
+      return false;
     }
+    return true;
+  }
+
+  set_value(value){
+    this.value = value;
+  }
+
+  buttonClick = (e) => {
+    for (let j = 0; j < this.fobjects.length; j++) {
+        this.fobjects[j].classList.remove('filter-button-active');
+    }
+    e.target.classList.add("filter-button-active");
+    this.set_value(e.target.dataset.value);
+    console.log(this.value);
+  }
+
+  initListener() {
+    for (let i = 0; i < this.fobjects.length; i++) {
+      this.fobjects[i].addEventListener("click", this.buttonClick);
+    }
+  }
 }
 
 class SmallFilter extends BaseFilter {
-    constructor(){
-        super();
-        this.type_buttons = new ButtonsInput("filter-button-small-filter", "type");
-    }
+  constructor(){
+    super();
+    this.type_buttons = new ButtonsInput("filter-button-small-filter", "type");
+  }
 }
 class BigFilter extends BaseFilter {
-    constructor(){
-      super()
-    }
+  constructor(){
+    super()
+  }
 }
 // DI данные для запросов
 class ServerDependancies {
-    constructor(){}
+  constructor(){}
 }
 // DI данные для запросов
 class HtmlClassesDependancies {
-    constructor(){}
+  constructor(){}
 }
 
 // Класс для общей инициализаии при рендере
 class Filter {
-    constructor(){
-        const small_filter = new SmallFilter();
-        // Инициализация маленького фильтра
-        // Инициализация большого фильтра
-    }
+  constructor(){
+    const small_filter = new SmallFilter();
+    // Инициализация маленького фильтра
+    // Инициализация большого фильтра
+  }
 }
 
 const filter = new Filter();
